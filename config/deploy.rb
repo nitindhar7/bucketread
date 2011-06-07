bd983638366f5a6274bb8d22e969cf9dee4d8c96
Nitin Dhar <nitindhar7@yahoo.com>
2011-06-07 12:38:14 -0400

    setup deployment and host. added dispatchers

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,39 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application,   "bucketread"
+set :repository,    "git@github.com:nitindhar7/bucketread.git"
+set :scm,           :git
+set :scm_command,   "/home2/basedata/opt/bin/git"
+set :local_scm_command, "git" 
+set :user,          "basedata"
+set :password,      "n5!GT@AB#dk"
+set :keep_releases, 5
+set :rake,          '/usr/bin/rake'
+set :branch,        'master'
+set :use_sudo,      false
+set :deploy_to,     "~/#{application}"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+role :app,          "basedataapp.com"
+role :web,          "basedataapp.com"
+role :db,           "basedataapp.com", :primary => true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+default_run_options[:pty]   = true
+ssh_options[:paranoid]      = false
+ssh_options[:forward_agent] = true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+desc "Add config symlinks"
+task :custom_symlinks do
+  run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
+end
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+desc "update permissions for project"
+task :update_project_permissions do
+  run "chmod -R u=rwx,go=rx /home2/basedata/bucketread"
+end
+
+desc "updated permissions for fgci"
+task :update_fcgi_permissions do
+  run "chmod 0755 #{release_path}/public/dispatch.*"
+end
+
+after "deploy:update_code", "custom_symlinks"
+after "custom_symlinks", "update_project_permissions"
+after "update_project_permissions", "update_fcgi_permissions"

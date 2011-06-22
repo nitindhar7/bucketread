7151550c4bd9dc654cafb655e9c740bb3eef4fff
Nitin Dhar <nitindhar7@yahoo.com>
2011-06-22 02:12:58 -0400

    added faq page action. updated footer

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ ActionController::Routing::Routes.draw do |map|
   map.authorize "authorize", :controller => :users, :action => :authorize
   map.api_authorize "authorize.:format", :controller => :users, :action => :authorize, :format => :format
   map.dashboard "dashboard", :controller => :dashboard
+  map.faq "faq", :controller => :dashboard, :action => :faq
 
   map.connect '/auth/failure', :controller => :users, :action => :failure
   map.connect "/auth/:provider", :controller => :users, :action => :blank

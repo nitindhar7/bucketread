6af42c1ecfec507993ec8904553644138df7de61
Nitin Dhar <nitindhar7@yahoo.com>
2011-06-27 04:38:30 -0400

    cleaned up js and css. updated nav styling

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -1,9 +1,21 @@
 $( document ).ready(function() {
-	hideMessage();
+	messagesSetup();
+	navSetup();
 });
+
+function messagesSetup()
+{
+	hideMessage();
+}
 
 function hideMessage() {
 	setTimeout(function() {
 		$( "#messages" ).fadeOut( 'slow' );
 	}, 3500);
+}
+
+function navSetup()
+{
+	$( ".tab:first" ).addClass( "first_tab" );
+	$( ".tab:last" ).addClass( "last_tab" );
 }

d631b2256386d383ff8c6d763b4cf2416de95e9f
Nitin Dhar <nitindhar7@yahoo.com>
2011-06-30 06:45:33 -0400

    cleaned up js

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -8,28 +8,6 @@ $( document ).ready(function() {
 		displayAddPageForm();
 	});
 	
-	$( "#top_control_filter_read_links" ).live("click", function() {
-		$.ajax({
-			url: "/dashboard/pages",
-			dataType: "script",
-			data: "status=1",
-			success: function( data ){
-				$( "#window" ).html( data );
-			}
-		});
-	});
-	
-	$( "#top_control_filter_unread_links" ).live("click", function() {
-		$.ajax({
-			url: "/dashboard/pages",
-			dataType: "script",
-			data: "status=0",
-			success: function( data ){
-				$( "#window" ).html( data );
-			}
-		});
-	});
-	
 	$( "#top_control_filter" ).live("change", function() {
 		var status_val = $( "#top_control_filter option:selected" ).val();
 		
